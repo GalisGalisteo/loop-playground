4fe847f29401d0dd0c3ad8d9b762bfba5a688f67
GalisGalisteo <galis.travelling@gmail.com>
2023-10-10 12:31:31 +0200

    finishing 11

--- mi-script.js
+++ mi-script.js
@@ -12,16 +12,21 @@
 
 let numeroInicial = 0;
 let numeroFinal = 10;
+let tocaHexagono = 4;
+
 // Elimina las 3 líneas de  código siguiente y codifica la solución
 
-dibujarCirculo();
-dibujarCirculo();
-dibujarCirculo();
+for (let i = numeroInicial; i < numeroFinal; i++) {
+    if (i == tocaHexagono) {
+        dibujarHexagono()
+    } else {
+        dibujarCirculo();
+    }
+}
 
 /**
  * BONUS: Modifica el bucle para introducir la variable 'tocaHexagono'. El bucle debe funcionar como antes, pero ahora, cuando nos encotremos en la iteración número 'tocaHexagono', se debe pintar un hexagono amarillo. Investiga el fichero "mi-script.js" para averiguar como podríamos pintar un hexágono
  */
-let tocaHexagono = 4;
 
 /**
  * Ejemplo: si numeroInicial=0, numeroFinal=10, y tocaHexagono=4, deberíamos pintar:
