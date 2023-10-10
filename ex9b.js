5c2d8c7b8f5bcfb40ef3f2c6e443302bf8959684
GalisGalisteo <galis.travelling@gmail.com>
2023-10-10 12:10:36 +0200

    finishing 9b

--- ex9b.js
+++ ex9b.js
@@ -12,15 +12,23 @@
  * 3. Si la siguiente letra que ha introducido el usuario se encuentra en la palabra del dia, PERO no es la posición correcta, mostrar por consola: "la letra {letra} es correcta, PERO se encuentra en una posición diferente"
  */
 
-let palabraDelDia= "bucle";
+let palabraDelDia = "bucle";
 let palabraIntroducida = "bueno";
 
-for (let i=0;i<palabraDelDia.length;i++) {
+for (let i = 0; i < palabraDelDia.length; i++) {
 
     let siguienteLetraCorrecta = palabraDelDia[i];
     let siguienteLetraIntroducida = palabraIntroducida[i];
 
     // Tu código a partir de aquí, dentro del bucle
+
+    if (siguienteLetraIntroducida === siguienteLetraCorrecta) {
+        console.log(`la letra ${palabraIntroducida[i]} es correcta`);
+    } else if (palabraDelDia.includes(siguienteLetraIntroducida)) {
+        console.log(`la letra ${palabraIntroducida[i]} es correcta, pero se encuentra en una posición diferente`);
+    } else {
+        console.log(`la letra ${palabraIntroducida[i]} no es correcta`);
+    }
 }
 
 // Resultado esperado: https://oscarm.tinytake.com/msc/ODc4MzY1N18yMjIwMzg5Mg
